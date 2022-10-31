d9233b6795b3d8e74060847b0b6c4cf92073b994
DongHyeonLee <jsleeg7279@naver.com>
2022-10-31 11:45:15 +0900

    경사하강법 실습

--- week9/diff.py
+++ week9/diff.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def numerical_diff(f, x):
@@ -13,3 +14,52 @@ def function_1(x):
 print(numerical_diff(function_1, 5))
 print(numerical_diff(function_1, 10))
 
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)  # x와 형상이 같은 배열을 생성
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h) 계산
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val  # 값 복원
+
+    return grad
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+    x_history = []
+
+    for i in range(step_num):
+        x_history.append(x.copy())
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x, np.array(x_history)
+
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+
+init_x = np.array([-3.0, 4.0])
+gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100)
+
+init_x = np.array([-3.0, 4.0])
+x, x_history = gradient_descent(function_2, init_x, lr=0.1, step_num=20)
+
+plt.plot([-5, 5], [0, 0], '--b')
+plt.plot([0, 0], [-5, 5], '--b')
+plt.plot(x_history[:, 0], x_history[:, 1], 'o')
+
+plt.xlim(-3.5, 3.5)
+plt.ylim(-4.5, 4.5)
+plt.xlabel("X0")
+plt.ylabel("X1")
+plt.show()
